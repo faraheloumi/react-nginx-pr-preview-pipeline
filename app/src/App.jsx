2e1aefd4e6a64b26ca5cf80800168606cd996eff
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-18 21:05:21 +0100

    add code

---
+++ app/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+export default function App(){ 
+  return <div style={{fontFamily:'sans-serif',padding:20}}>
+    <h1>Preview demo</h1>
+    <p>This is a PR preview build.</p>
+  </div>;
+}
