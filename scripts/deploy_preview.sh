5d6863f622a797d513a7da5b5a4904b5cbc9d0e8
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-19 14:02:31 +0100

    Update code

--- scripts/deploy_preview.sh
+++ scripts/deploy_preview.sh
@@ -7,6 +7,6 @@ docker rm pr-$PR_NUM || true
 docker run -d --name pr-$PR_NUM -p $PORT:80 ghcr.io/<user>/<repo>/web:pr-$PR_NUM
 # générer config NGINX pour ce PR
 cp nginx/pr-template.conf /etc/nginx/sites-enabled/pr-$PR_NUM.conf
-sed -i "s/PR_HOSTNAME/pr-$PR_NUM.farah.duckdns.org/g" /etc/nginx/sites-enabled/pr-$PR_NUM.conf
+sed -i "s/PR_HOSTNAME/pr-$PR_NUM.farahelloumi.duckdns.org/g" /etc/nginx/sites-enabled/pr-$PR_NUM.conf
 sed -i "s/PR_PORT/$PORT/g" /etc/nginx/sites-enabled/pr-$PR_NUM.conf
 sudo nginx -s reload
